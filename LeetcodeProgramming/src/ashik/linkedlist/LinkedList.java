50f2ad71526482ba793f3b7b626da967a4d35095
ashik <ashikplus570@gmail.com>
2025-01-12 11:58:13 +0600

    LinkedList

---
+++ LeetcodeProgramming/src/ashik/linkedlist/LinkedList.java
@@ -0,0 +1,40 @@
+package ashik.linkedlist;
+
+public class LinkedList {
+
+	static Node head;
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    
+    static void printList(Node node)
+    {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+	public static void main(String[] args) {
+
+		LinkedList.head = new Node(1);
+		LinkedList.head.next = new Node(1);
+		LinkedList.head.next.next = new Node(4);
+		LinkedList.head.next.next.next = new Node(20);
+
+        System.out.println("Given linked list");
+        LinkedList.printList(LinkedList.head);
+        
+//        LinkedList.deleteDuplicates(LinkedList.head);
+	}
+
+}
